ab2a2fc331d6aac5f46ade9485f13871f13f7b8a
RafiKastner <rafikastner@gmail.com>
2025-10-24 22:49:01 -0500

    open-git fish function

---
+++ fish/functions/open-git.fish
@@ -0,0 +1,21 @@
+#cd (tmux run "echo #(pane_start_path")a
+function open-git
+    if test -z (git remote)
+        echo "This repository has no remotes"
+        return
+    end
+
+    set url (git remote get-url origin)
+
+    if string match -gq "*github.com*" $url
+        if string match -gq "git@*" $url
+            set url (string replace "git@" "" $url)
+            set url (string replace ":" "/" "$url")
+            echo $url
+            set url "https://$url"
+        end
+        open "$url"
+    else
+        echo "This repository is not hosted on GitHub"
+    end
+end
